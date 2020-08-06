0923f827d3be593b80ed0206ebf9fcde55e25af4
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-08-06 13:59:06 -0300

    Added enqueue and size methods with its test

--- queues/priority_queue.py
+++ queues/priority_queue.py
@@ -1,3 +1,9 @@
+"""
+Based on https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-priority-queue-class
+"""
+import pytest
+
+
 class PriorityQueue(object):
     def __init__(self):
         """
@@ -12,20 +18,27 @@ class PriorityQueue(object):
         will be dequeued before all the others.
 
         ex.:
-        >>> priority_queue = PriorityQueue()
-        >>> priority_queue.add([['Human', 2], ['dog', 2], ['rabbit', 2]])
-        >>> priority_queue.data = [['Human', 2], ['dog', 2], ['rabbit', 2]]
+        # >>> priority_queue = PriorityQueue()
+        # >>> priority_queue.enqueue([['Human', 2], ['dog', 2], ['rabbit', 2]])
+        # >>> priority_queue.data = [['Human', 2], ['dog', 2], ['rabbit', 2]]
 
-        >>> priority_queue.add(["Cat", 1])
-        >>> priority_queue.data = [["Cat", 1]['Human', 2], ['dog', 2], ['rabbit', 2]]
+        # >>> priority_queue.enqueue(["Cat", 1])
+        # >>> priority_queue.data = [["Cat", 1]['Human', 2], ['dog', 2], ['rabbit', 2]]
         """
         self.data = []
 
-    def enqueue(self):
+    def enqueue(self, item):
         """
         Add an item with priority to the queue
         """
-        pass
+        if not self.data:
+            self.data.append(item)
+        else:
+            position = 0
+            for element in self.data:
+                if element[1] <= item[1]:
+                    position += 1
+            self.data.insert(position, item)
 
     def dequeue(self):
         """
@@ -37,7 +50,7 @@ class PriorityQueue(object):
         """
         Return number of items in the queue
         """
-        pass
+        return len(self.data)
 
     def peek(self):
         """
@@ -51,3 +64,36 @@ class PriorityQueue(object):
         """
         pass
 
+
+@pytest.fixture
+def priority_queue():
+    priority_queue = PriorityQueue()
+    return priority_queue
+
+
+class TestPriorityQueue(object):
+    def test_enqueue(self, priority_queue):
+        """
+        Given an item with priority,
+        when enqueued is called,
+        must add the item in the correct position based on priority
+        """
+        priority_queue.enqueue(['item1', 1])
+        priority_queue.enqueue(['item2', 1])
+        priority_queue.enqueue(['item3', 2])
+        priority_queue.enqueue(['item4', 1])
+        priority_queue.enqueue(['item5', 3])
+        assert priority_queue.data == [['item1', 1], ['item2', 1], ['item4', 1], ['item3', 2], ['item5', 3]]
+
+    def test_size(self, priority_queue):
+        """
+        Given a priority queue,
+        when size is called,
+        must return the number of items in the queue.
+        """
+        priority_queue.enqueue(['item1', 1])
+        priority_queue.enqueue(['item2', 1])
+        priority_queue.enqueue(['item3', 2])
+        priority_queue.enqueue(['item4', 1])
+        priority_queue.enqueue(['item5', 3])
+        assert len(priority_queue.data) == 5
